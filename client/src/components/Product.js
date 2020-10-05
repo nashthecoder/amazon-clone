4159d04e62af53d3f200deccf57ce5200d508889
NashTheCoder <naijeria@gmail.com>
2020-10-05 21:38:41 +0300

    product rating error fixed

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Product.css';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+import StarOutlineIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
 import { useStateValue } from './StateProvider';
 
 function Product ({ id, title, image, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
+  let halfRating = (rating - Math.floor(rating)) * 10;
+    let outline = 0;
+    halfRating > 0 ? outline = (5 - Math.ceil(rating)) : outline = (5 - Math.floor(rating))
 
   // Add item to basket
    const addToBasket = () => {
@@ -27,9 +33,28 @@ function Product ({ id, title, image, price, rating }) {
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        {/* <div className="product__rating">
-          {Array(rating).fill()​​.map((_, i) => ( <span key={i} role="img" aria-label="star"> ⭐️ </span>))}
-        </div> */}
+        <div className="product__rating">
+        {
+                            Array(Math.floor(rating))
+                                .fill()
+                                .map((_, index) => (
+                                    <StarIcon key={index} />
+                                ))
+                        }
+                        {
+                            (halfRating > 0) ? <StarHalfIcon /> : <></>
+                        }
+                        {
+                            outline > 0 ? (
+                                Array(outline)
+                                    .fill()
+                                    .map((_, index) => (
+                                        <StarOutlineIcon key={index} />
+                                    ))
+                            )
+                                : ""
+                        }
+        </div> 
       </div>
       <img src={image} alt="" />
 
@@ -39,3 +64,8 @@ function Product ({ id, title, image, price, rating }) {
 }
 
 export default Product;
+
+
+
+
+
