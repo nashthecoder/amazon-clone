3864d193b796edfc624068b7e39d46884040b865
NashTheCoder <naijeria@gmail.com>
2020-10-07 00:19:26 +0300

    Updates

---
+++ precache-manifest.75c8e7fbbf0008474ebf3dac1de31851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc7de8a1886afc85258966acd9d4abd",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "7bab6ff0cc3bd6e4546b",
+    "url": "/amazon-clone/static/css/main.1e8d1341.chunk.css"
+  },
+  {
+    "revision": "e68e54b49038a3e54d5f",
+    "url": "/amazon-clone/static/js/2.a8f2a058.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/amazon-clone/static/js/2.a8f2a058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bab6ff0cc3bd6e4546b",
+    "url": "/amazon-clone/static/js/main.520a185c.chunk.js"
+  },
+  {
+    "revision": "932585abc157c3fda0be",
+    "url": "/amazon-clone/static/js/runtime-main.03b86076.js"
+  }
+]);
